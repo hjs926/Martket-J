a1b0e03aa16312af76a1a964da4e9e848d56a73f
hjs926 <joonsun926@gmail.com>
2022-09-01 17:42:56 +0900

    update

--- server/index.js
+++ server/index.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(
+    "mongodb+srv://js96:1234@market-j.iewrcuc.mongodb.net/?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("MongoDB Connectd..."))
+  .catch((err) => console.log(err));
 
 app.get("/", (req, res) => res.send("Hello"));
 
