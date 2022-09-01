27ab91831c1ac748323284e7b6c039d8dc961bc9
hjs926 <joonsun926@gmail.com>
2022-09-01 22:24:28 +0900

    server_dev_update

--- server/index.js
+++ server/index.js
@@ -1,16 +1,37 @@
 const express = require("express");
 const app = express();
+
 const port = 5000;
+const { User } = require("./models/User");
+const bodyParser = require("body-parser");
+
+const config = require("./config/key");
+
+//application/x-www-form-urlencoded << 이런 데이터를 분석해서 가져올수 있게 해주는 것
+app.use(bodyParser.urlencoded({ extended: true }));
+//application/json 타입으로 된 것을 분석해서 가져올수 있게 해준 것
+app.use(bodyParser.json());
+
 const mongoose = require("mongoose");
 
 mongoose
-  .connect(
-    "mongodb+srv://js96:1234@market-j.iewrcuc.mongodb.net/?retryWrites=true&w=majority",
-    { useNewUrlParser: true, useUnifiedTopology: true }
-  )
-  .then(() => console.log("MongoDB Connectd..."))
+  .connect(config.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log(err));
+app.get("/", (req, res) => res.send("Hello world 안녕"));
 
-app.get("/", (req, res) => res.send("Hello"));
+//회원가입을 위한 정보들을 client에서 가져오면
+//받은 정보를 DB에 넣어준다
+app.post("r", (req, res) => {
+  //회원가입 할때 필요한 정보들을 클라이언트에서 가져오면
+  //그것들을 데이터베이스에 넣어준다.
+  const user = new User(req.body);
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
