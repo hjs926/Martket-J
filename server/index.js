535867a2e8c1ecf9ea4a5d22c958e96c4844a0ad
hjs926 <joonsun926@gmail.com>
2022-09-01 15:38:17 +0900

    server_update

--- server/index.js
+++ server/index.js
@@ -1,1 +1,7 @@
-//hello
+const express = require("express");
+const app = express();
+const port = 5000;
+
+app.get("/", (req, res) => res.send("Hello"));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
