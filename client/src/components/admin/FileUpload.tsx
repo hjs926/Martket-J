ffe28a2afd1b2a2ec17dff0a2d30537bd5145ab7
hjs926 <joonsun926@gmail.com>
2022-09-20 20:18:29 +0900

    업로드
    다 작성해도 모두 작성해달라고 뜸

--- client/src/components/admin/FileUpload.tsx
+++ client/src/components/admin/FileUpload.tsx
@@ -2,22 +2,24 @@ import React, { useState } from "react";
 import Dropzone from "react-dropzone";
 import axios, { AxiosResponse } from "axios";
 
-function FileUpload(props) {
+function FileUpload(props: any) {
   const [Images, setImages] = useState([] as any);
 
   const handleDropImage = (files: any) => {
     let formData = new FormData();
 
-    const config: any = {
-      header: { "content-type": "multipart/form-data" },
+    const config = {
+      headers: { "content-type": "multipart/form-data" },
     };
     formData.append("file", files[0]);
     axios
       .post("http://localhost:4000/api/product/image", formData, config)
-      .then((response: AxiosResponse<any, any>) => {
+      .then((response) => {
         if (response.data.success) {
+          console.log(response.data);
           setImages([...Images, response.data.filePath]);
-          props.movefunction([...Images, response.data.filePath]);
+          props.moveFunction();
+          // props.movefunction([...Images, response.data.filePath]);
         } else {
           alert("파일 저장 실패.");
         }
@@ -29,13 +31,13 @@ function FileUpload(props) {
 
   const handleDeleteImage = (image: any) => {
     const currentIndex = Images.indexOf(image);
+    console.log("currentIndex", currentIndex);
     let newImages = [...Images];
     newImages.splice(currentIndex, 1);
     setImages(newImages);
-    props.movefunction(newImages);
     // 내가 클릭한 이미지의 index 번호~1까지 지워주겠다는 의미
-    // ex) index 번호 0을 클릭했다면 0부터1까지 지우는거나 하나만 지움
-    // ex) index 번호 2를 클릭했다면 2부터3까지 지우는거나 하나만 지움
+    // ex) newImages.splice(0,2) -> [0,1,2] 중 0부터 1까지 [0,1] 2개 삭제
+    props.moveFunction(newImages);
   };
 
   return (
@@ -48,7 +50,7 @@ function FileUpload(props) {
               height: 240,
               border: "1px solid lightgray",
               display: "flex",
-              alignSelf: "center",
+              alignItems: "center",
               justifyContent: "center",
             }}
             {...getRootProps()}
--- server/routes/product.js
+++ server/routes/product.js
@@ -1,6 +1,6 @@
 import express from "express";
 import multer from "multer";
-
+const Product = "../models/Product";
 const router = express.Router();
 
 let storage = multer.diskStorage({
@@ -28,4 +28,12 @@ router.post("/image", (req, res) => {
   });
 });
 
+router.post("/product", (req, res) => {
+  //body에서 받은 정보를 DB에 저장
+  const product = new Product(req.body);
+  product.save((err) => {
+    if (err) return res.status(400).json({ success: false, err });
+    return res.status(200).json({ success: true });
+  }); // 넣은 정보들이 product 안에 다 저장 됨
+});
 export default router;
