f116481a94c9448bc4394d74144abc5a1e30ab30
hjs926 <joonsun926@gmail.com>
2022-09-29 02:22:45 +0900

    update

--- client/src/components/product/products.tsx
+++ client/src/components/product/products.tsx
@@ -1,47 +1,165 @@
-import { useQuery } from "@tanstack/react-query";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
-import { QueryKeys, restFetcher } from "../../queryClient";
+import { useEffect, useState } from "react";
+import axios from "../../axios/axios";
 import { Product } from "../../type";
-import ProductItem from "./product-Item";
 
-const ProductContainer = styled.ul`
-  display: flex;
-  margin-top: 20px;
-  flex-wrap: wrap;
-  width: 100%;
+const MainSection2 = styled.section`
   max-width: 100%;
+  padding-left: 200px;
+  padding-right: 100px;
+  margin-top: 80px;
+  display: flex;
+  flex-direction: column;
+
+  .main-section-2tit {
+    text-align: center;
+  }
+
+  .main-section-2contents {
+    min-width: 1000px;
+    margin-top: 50px;
+
+    ul {
+      width: 100%;
+      display: flex;
+      flex-wrap: wrap;
+      li {
+        box-sizing: border-box;
+        max-width: 13%;
+      }
+    }
+  }
 `;
 
-// category: "men's clothing"
-// description: "great outerwear jackets for Spring/Autumn/Winter.......
-// id: 3
-// image:"https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg"
-// price: 55.99
-// rating: {
-//   count: 500,
-//   rate: 4.7
-// title: "Mens Cotton Jacket"
-// }
+const Products = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
 
-const ProductList = () => {
-  // const { data, isLoading } = useQuery<Product2[]>([QueryKeys.PRODUCTS], () =>
-  //   restFetcher({
-  //     method: "GET",
-  //     path: "/api/product",
-  //   })
-  // );
-  // if (isLoading) return <h2>Loding...</h2>;
-  // if (!data) return null;
+const Product_Image = styled.div`
+  width: 380px;
+  height: 380px;
+  border-radius: 10px;
+  margin-bottom: 16px;
+`;
 
-  // console.log(data);
+const Product_Image2 = styled.img`
+  width: 100%;
+  height: 100%;
+  border-radius: 10px;
+  border: 1px solid #c4c4c4;
+`;
+
+const Store = styled.div`
+  margin-bottom: 10px;
+`;
+
+const Store2 = styled.span`
+  font-size: 16px;
+  line-height: 22px;
+  color: #767676;
+`;
+
+const Product_Name = styled.div`
+  margin-bottom: 10px;
+`;
+
+const Product_Name2 = styled.div`
+  font-size: 18px;
+  line-height: 22px;
+  color: #000000;
+`;
+
+const Product_Price = styled.div``;
+
+const Price = styled.span`
+  font-size: 24px;
+  font-weight: bold;
+  line-height: 30px;
+  color: #000000;
+`;
+
+const Unit = styled.span`
+  font-size: 16px;
+  line-height: 20px;
+  color: #000000;
+`;
+
+const PRODUCTMORE_URL = "/api/product/products";
+
+const ProductPage = () => {
+  const [products, setProducts] = useState<Product[]>([]);
+  const [Skip, setSkip] = useState(0);
+  const [Limit, setLimit] = useState(8);
+  const [postSize, setPostSize] = useState(0);
+
+  useEffect(() => {
+    let body = {
+      skip: Skip,
+      limit: Limit,
+    };
+    getProducts(body);
+  }, []);
+
+  const getProducts = (body: any) => {
+    axios.post(PRODUCTMORE_URL, body).then((response) => {
+      if (response.data.success) {
+        if (body.loadMore) {
+          console.log("상품목록: ", response.data);
+          setProducts([...products, ...response.data.productInfo]);
+        } else {
+          setProducts(response.data.productInfo);
+        }
+        setPostSize(response.data.postSize);
+      } else {
+        alert("상품 불러오기 실패");
+      }
+    });
+  };
+
+  const handleProductMore = () => {
+    let skip = Skip + Limit;
+
+    let body = {
+      skip: skip,
+      limit: Limit,
+      loadMore: true,
+    };
+    getProducts(body);
+    setSkip(skip);
+  };
+
+  const renderCards = products.map((product: any, index) => {
+    console.log("product 정보", product);
+    return (
+      <Products>
+        <Product_Image>
+          <Product_Image2
+            key={index}
+            src={`http://localhost:4000/${product.images[0]}`}
+          />
+        </Product_Image>
+        <Product_Name>
+          <p>{product.description}</p>
+        </Product_Name>
+        <Product_Price>
+          <p>{product.price}원</p>
+        </Product_Price>
+      </Products>
+    );
+  });
 
   return (
-    <ProductContainer>
-      {/* {data.slice(0, 7)?.map((product) => (
-        <ProductItem {...product} key={product._id} />
-      ))} */}
-    </ProductContainer>
+    <>
+      <div>{renderCards}</div>
+
+      {postSize >= Limit && (
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <button onClick={handleProductMore}>더보기</button>
+        </div>
+      )}
+    </>
   );
 };
-
-export default ProductList;
+export default ProductPage;
