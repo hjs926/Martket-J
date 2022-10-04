da501584d57b8bee5aa8f66e80519ecf2a4e3c93
hjs926 <joonsun926@gmail.com>
2022-10-04 11:07:52 +0900

    update

--- client/src/pages/products/[id].tsx
+++ client/src/pages/products/[id].tsx
@@ -1,26 +1,27 @@
 import { useQuery } from "@tanstack/react-query";
 import { useParams } from "react-router";
 import ProductDetail from "../../components/product/detail";
+import Detail_product from "../../components/product/detail_product";
 import { QueryKeys, restFetcher } from "../../queryClient";
 import { Product } from "../../type";
 
 const ProductDetailPage = () => {
-  const { id } = useParams();
-  const { data } = useQuery<Product>([QueryKeys.PRODUCTS, id], () =>
-    restFetcher({
-      method: "GET",
-      path: `/products/${id}`,
-    })
-  );
-  if (!data) return null;
-  console.log("상세목록페이지입니다.");
-  console.log("key", [QueryKeys.PRODUCTS, id]);
-  console.log("data", data);
+  // const { id } = useParams();
+  // const { data } = useQuery<Product>([QueryKeys.PRODUCTS, id], () =>
+  //   restFetcher({
+  //     method: "GET",
+  //     path: `/products/${id}`,
+  //   })
+  // );
+  // if (!data) return null;
+  // console.log("상세목록페이지입니다.");
+  // console.log("key", [QueryKeys.PRODUCTS, id]);
+  // console.log("data", data);
 
   return (
     <div>
       <br />
-      <ProductDetail {...data} />
+      <Detail_product />
     </div>
   );
 };
